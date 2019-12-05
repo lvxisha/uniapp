fb98af8c134cfc7f269437ee58961cbf7793eccd
lvxisha <1664027033@qq.com>
2019-12-05 08:43:52 +0800

    新增页面

---
+++ pages/removeList/removeList.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>新增页面</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
